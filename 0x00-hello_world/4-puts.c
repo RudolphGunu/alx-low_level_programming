fd221b1881e1a06bf76d5bd9f64b545b08c10f8d
RudolphGunu <rudolphgunu@gmail.com>
2023-08-10 18:16:55 -0700

    u

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ *
+ *Description: This program uses puts to print out a string
+ * describe tge parameter
+ *
+ *Return :Always 0
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
