533a069cf1d8ad36cf355a0b2c57ecef960d6f54
RudolphGunu <rudolphgunu@gmail.com>
2023-08-16 01:32:35 -0700

    r

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints 10x the alphabet
+ *
+ *Description: prints alphabet x10
+ *
+ *Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+  int c;
+  int j;
+    for (c = 0; c <= 10; ++c)
+      {
+	for (j = 'a'; j <= 'z'; ++j)
+	  putchar(j + '0');
+      }
+      putchar('\n');
+    
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,8 @@
 /* 0 */
 
 void print_alphabet(void);
+
+/* 2 */
+void print_alphabet_x10(void);
+
 #endif  /* MAIN_H */
