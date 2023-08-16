30ccbb85d1d22c447cb760fec94927ee27207467
RudolphGunu <rudolphgunu@gmail.com>
2023-08-16 03:10:59 -0700

    e

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -1,24 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * test_islower - Test the _islower function
+ *
+ * @n: Number to pass to _islower function
+ */
+void test_islower(int n)
+{
+	int r;
+
+	r = _islower(n);
+	putchar(r + '0');
+	putchar('\n');
+}
 
 /**
- *main - check the code
+ * main - check the code for Holberton School students.
  *
- *Return: Always 0
- *
- *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-int r;
-
-r = _islower('H');
-putchar(r + '0');
-r = _islower('o');
-putchar(r + '0');
-r = _islower(108);
-putchar(r + '0');
-putchar('\n');
-return (0);
+	test_islower('H');
+	return (0);
 }
