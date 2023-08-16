e238b9ed9219b27dcfdc5cddbc52f32d25843f22
RudolphGunu <rudolphgunu@gmail.com>
2023-08-16 01:41:32 -0700

    rR

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,15 +11,14 @@
 
 void print_alphabet_x10(void)
 {
-  int c;
-  int j;
-    for (c = 0; c < 10; ++c)
-      {
-	for (j = 'a'; j <= 'z'; ++j)
-	  {
-	  putchar(j);
-	  }
-      putchar('\n');
-      }
-    
+int c;
+int j;
+for (c = 0; c < 10; ++c)
+{
+for (j = 'a'; j <= 'z'; ++j)
+{
+putchar(j);
+}
+putchar('\n');
+}
 }
