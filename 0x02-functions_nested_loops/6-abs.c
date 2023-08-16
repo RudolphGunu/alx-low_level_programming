6ecbdc2660abb7146097ac8157b797f5c2862dea
RudolphGunu <rudolphgunu@gmail.com>
2023-08-16 09:03:37 -0700

    ww

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_abs - prints theabsolute value of an intiger
+ *Description: prints the absolute value of an int
+ *@i: the value to test
+ *Return: The absolute value of i
+ *
+ *
+ */
+int _abs(int i)
+{
+return (i < 0 ? -1 : 1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -19,7 +19,26 @@ int _isalpha(int c);
 
 /*  5  */
 int print_sign(int n);
+/* 6 */
+int _abs(int);
 
+/* 7 */
+int print_last_digit(int);
+
+/* 8 */
+void jack_bauer(void);
+
+/* 9 */
+void times_table(void);
+
+/* 10 */
+int add(int, int);
+
+/* 11 */
+void print_to_98(int n);
+
+/* 12 */
+void print_times_table(int n);
 
 
 #endif  /* MAIN_H */
