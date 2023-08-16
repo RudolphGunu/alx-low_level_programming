559b0965eab36c58ffb8b4611df1494f5f8fb55e
RudolphGunu <rudolphgunu@gmail.com>
2023-08-16 11:49:15 -0700

    rr

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *jack_bauer - prints minutes
+ *Description: prints minutes
+ *Return: 0
+ *
+ *
+ */
+void jack_bauer(void)
+{
+int h;
+int m;
+for (h = 0; h <24; ++h)
+{
+for (m = 0; m < 60; ++m)
+{
+_putchar('0' + h / 10);
+_putchar('0' + h % 10);
+_putchar(':');
+_putchar('0' + m / 10);
+_putchar('0' + m % 10);
+_putchar('\n');
+}
+}
+}
