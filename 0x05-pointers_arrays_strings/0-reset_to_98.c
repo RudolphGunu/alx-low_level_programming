c4726140ca9a4490616100c13b3bc79ea80574b7
RudolphGunu <rudolphgunu@gmail.com>
2023-08-21 21:26:35 -0700

    yt

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *reset_to_98 - resets to 98
+ *
+ *Description: resets to 98
+ *
+ *Return: 0
+ *
+ */
+
+void reset_to_98(int *n)
+{
+if (n)
+*n = 98;
+}
